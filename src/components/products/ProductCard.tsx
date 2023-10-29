c20469b641c543e6f1943790c580e9d994b40f94
“Marco <marco.pianaroli96@gmail.com>
2023-10-29 22:45:09 +0100

    refactor:productCard: eliminates MUI components

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,9 +1,5 @@
 import { Product } from "../../types";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/cartSlice";
 import { increment } from "../../redux/cartCountSlice";
@@ -22,48 +18,22 @@ export default function ProductCard(props: ProductCardProps) {
   }
 
   return (
-    <Card
-      sx={{
-        padding: 2,
-        width: 345,
-        height: "100%",
-        backgroundColor: "  #fed7aa",
-        boxShadow: 3,
-        ":hover": { boxShadow: 10 },
-      }}
-    >
-      <CardMedia
-        component="img"
-        alt={product.title}
-        height="140"
-        image={product.image}
-        sx={{ height: 140, objectFit: "contain" }}
-      />
-      <CardContent className="flex flex-col">
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          className="h-10 overflow-scroll"
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          className="h-20 overflow-scroll"
-        >
-          {product.description}
-        </Typography>
-        <Typography variant="h5" color="text-secondary" className="self-center">
-          $ {product.price}
-        </Typography>
-      </CardContent>
-      <CardActions className="flex flex-row justify-end">
-        <button className="self-end" onClick={() => addToCartHandler(product)}>
-          Add To Cart
-        </button>
-      </CardActions>
-    </Card>
+    <article className=" flex flex-col justify-between items-center p-2 w-86 h-full bg-[#fed7aa] shadow hover:shadow-lg rounded-lg">
+      <figure className="w-full  h-36 ">
+        <img
+          className="object-contain h-full w-full "
+          src={product.image}
+          alt={product.title}
+        />
+      </figure>
+      <h3 className="text-xl  ">{product.title}</h3>
+      <p className="text-xs h-20  overflow-scroll">{product.description}</p>
+      <h4 color="text-secondary" className="self-center">
+        $ {product.price}
+      </h4>
+      <button className="mx-auto" onClick={() => addToCartHandler(product)}>
+        Add To Cart
+      </button>
+    </article>
   );
 }
