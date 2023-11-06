37983d0cb472a52c41228eda1c15dc7f109e5fc2
“Marco <marco.pianaroli96@gmail.com>
2023-11-06 19:27:24 +0100

    fixes overflow on drawer component

--- src/components/products/Cart/CartDrawer.tsx
+++ src/components/products/Cart/CartDrawer.tsx
@@ -4,7 +4,7 @@ import { closeDrawer } from "../../../redux/drawerSlice";
 import { RootState } from "../../../redux/store";
 import CartComponent from "./CartComponent";
 
-const Drawer = () => {
+export default function Drawer  ()  {
   const cartCount = useSelector((state: RootState) => state.cartCount);
   const cartItems = useSelector((state: RootState) => state.cart);
   const dispatch = useDispatch();
@@ -40,56 +40,39 @@ const Drawer = () => {
     };
   
 
+      
   return (
     <>
-      {/* Background overlay */}
-      <div
-        className={`fixed inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-sm ${
-          isOpen ? "block" : "hidden"
-        }`}
-      ></div>
-
-      {/* Drawer */}
-      <div
-        ref={drawerRef}
-        className={`fixed right-0 top-0 z-50 h-full  w-screen overflow-scroll transform rounded-lg border-4 border-orange-700 bg-orange-200 transition-transform lg:w-2/4 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <div className="flex h-16 items-center justify-center bg-orange-600">
+      <div className={`fixed h-screen inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-sm ${isOpen ? "block" : "hidden"}`}></div>
+      <div ref={drawerRef} className={`fixed right-0 top-0 z-50 h-full w-full sm:w-screen  transform rounded-lg border-4 border-orange-700 bg-orange-200 transition-transform lg:w-2/4 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <header className="flex p-4 items-center justify-center bg-orange-600">
           <h2 className="self-center">My cart: {cartCount}</h2>
-        </div>
-
-        {/* Close button */}
-        <button
-          className="absolute right-2 top-2"
-          onClick={() => dispatch(closeDrawer())}
-        >
-          X
-        </button>
-
-        {/* Drawer content */}
-        <div className="flex min-h-full flex-col items-center bg-orange-300 p-4">
+        </header>
+        <button className="absolute right-2 top-2" onClick={() => dispatch(closeDrawer())}>X</button>
+        <div className="flex h-full flex-col items-center bg-orange-300 p-4">
           {cartCount === 0 ? (
             <>
               <h2 className="text-2xl">{EMPTY_CART_TEXT.title}</h2>
               <span>{EMPTY_CART_TEXT.message}</span>
             </>
           ) : (
-            <ul className="h-72 min-h-full w-full items-center bg-orange-300 overflow-auto">
+            <div className="w-full items-center bg-orange-300 overflow-auto">
+            <ul >
               {cartItems.map((item, index) => (
-                <CartComponent item={item} index={index} />
+                <CartComponent key={index} item={item} index={index} />
               ))}
-            </ul>
+            </ul></div>
           )}
-      </div>
-      <div className="flex flex-col   bottom-4 h-1/4 w-full bg-orange-300">
-        <h3 id='price' className=" text-3xl  p-4  self-center ">
-          Total price  ${calculateTotalPrice()} </h3>
+        <div className="flex flex-col bottom-4 min-h-2/3 w-full bg-orange-300">
+          <h3 id='price' className="text-3xl p-4 self-center">
+            Total price ${calculateTotalPrice()}
+          </h3>
           <button className="rounded-full bg-orange-500 w-fit self-center p-3 hover:bg-orange-600">Checkout</button>
-      </div>
         </div>
+        </div>
+      </div>
     </>
   );
-};
-export default Drawer;
+
+
+              }
