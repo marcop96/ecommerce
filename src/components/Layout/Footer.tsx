40ba16667bf6fb7aa8e1048a8a9671f4fc297d89
“Marco <marco.pianaroli96@gmail.com>
2023-11-06 09:07:06 +0100

    refactor:Footer: recreates footer without MUI

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,64 +1,39 @@
-// import FacebookIcon from "@mui/icons-material/Facebook";
-// import TwitterIcon from "@mui/icons-material/Twitter";
-// import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
-// export default function Footer() {
-//   return (
-//     <footer className="h-48 bg-orange-700 p-5 text-white">
-//       <Container>
-//         <Grid container spacing={2}>
-//           <Grid item xs={12} sm={6} md={3}>
-//             <Typography variant="h6" style={{ color: "white" }}>
-//               Company
-//             </Typography>
-//             <Link href="#" style={{ color: "white" }}>
-//               About Us
-//             </Link>{" "}
-//             <br />
-//             <Link href="#" style={{ color: "white" }}>
-//               Contact Us
-//             </Link>{" "}
-//             <br />
-//             <Link href="#" style={{ color: "white" }}>
-//               FAQ
-//             </Link>
-//           </Grid>
-//           <Grid item xs={12} sm={6} md={3}>
-//             <Typography variant="h6" style={{ color: "white" }}>
-//               Support
-//             </Typography>
-//             <Link href="#" style={{ color: "white" }}>
-//               Customer Support
-//             </Link>
-//             <br />
-//             <Link href="#" style={{ color: "white" }}>
-//               Terms & Conditions
-//             </Link>
-//             <br />
-//             <Link href="#" style={{ color: "white" }}>
-//               Privacy Policy
-//             </Link>
-//           </Grid>
-//           <Grid item xs={12} md={6}>
-//             <Typography variant="h6" style={{ color: "white" }}>
-//               Connect With Us
-//             </Typography>
-//             <IconButton href="https://google.com">
-//               <FacebookIcon htmlColor="white" />
-//             </IconButton>
-//             <IconButton>
-//               <TwitterIcon htmlColor="white" />
-//             </IconButton>
-//             <IconButton>
-//               <LinkedInIcon htmlColor="white" />
-//             </IconButton>
-//           </Grid>
-//         </Grid>
-//       </Container>
-//     </footer>
-//   );
-// }
+export default function  Footer(){
+  return (
+    <footer className="bg-orange-700 text-white py-6">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex flex-col space-y-4">
+          <h2 className="text-xl font-bold">Company Name</h2>
+          <p>123 E-commerce Street</p>
+          <p>City, State, 12345</p>
+          <p>Email: info@company.com</p>
+        </div>
+        <div className="flex space-x-6">
+          <div>
+            <h3 className="text-lg font-semibold">Information</h3>
+            <ul className="mt-2 space-y-2">
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">Shop</h3>
+            <ul className="mt-2 space-y-2">
+              <a href="/">Home</a>
+              <a href='/products'>Shop</a>
+              <a href='FAQ'>FAQ</a>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="text-center text-sm mt-4">
+        &copy; {new Date().getFullYear()} Marco Shop All Rights Reserved.
+      </div>
+    </footer>
+  );
+};
 
-export default function Footer() {
-  return <h2>footer</h2>;
-}
+
