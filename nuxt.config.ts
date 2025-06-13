6cc84728c836f00027a481099c69170eca3ae05b
Marco Pianaroli <marco.pianaroli96@gmail.com>
2025-06-13 12:23:06 +0200

    Update nuxt.config.ts to include Supabase configuration and comment out Stripe keys; add @supabase/supabase-js dependency in package.json and pnpm-lock.yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,8 +17,11 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      stripeKey: process.env.STRIPE_PUBLIC_KEY
+      //stripeKey: process.env.STRIPE_PUBLIC_KEY,
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     },
-    stripeSecret: process.env.STRIPE_SECRET_KEY
+    //stripeSecret: process.env.STRIPE_SECRET_KEY,
+    supabaseServiceKey: process.env.SUPABASE_SERVICE_ROLE_KEY
   }
 })
